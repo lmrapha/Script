333003f0315675bbb05a3ad0a994d9eae85a51f5
rkyl <raphaallemos@gmail.com>
2022-10-31 11:32:17 -0300

    Script

---
+++ Script.java
@@ -0,0 +1,12 @@
+public class Script {
+
+    //Script para listar o número de arquivos abc e utilizar com rabcdasm.
+
+    int numero = 0;
+
+    public static void main(String[] args) {
+        for (int numero = 0; numero <= 6381;numero++) {
+            System.out.println("rabcdasm "+"Habbo-"+numero+".abc");
+        }
+    }
+}
